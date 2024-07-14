fe51bc7ec6a7790a0b7393055987fe257c786cbc
Kimin <71866731+inhalin@users.noreply.github.com>
2024-07-15 00:10:28 +0900

    chore: init project

---
+++ src/main/java/com/example/dddsimpleboard/DddSimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddsimpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddSimpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddSimpleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dddsimpleboard/DddSimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddsimpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddSimpleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
